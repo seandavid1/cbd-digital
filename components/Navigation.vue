2b7ab591e8dd35b710f359b30cadfa80dc7f094c
seandavid1 <seandavid1@gmail.com>
2019-02-12 13:50:17 -0500

    Add files via upload

---
+++ components/Navigation.vue
@@ -0,0 +1,81 @@
+<template>
+  <b-navbar
+    v-b-scrollspy:nav-scroller
+    toggleable="md"
+    type="light"
+    variant="primary-inverse"
+    fixed="top">
+
+    <b-navbar-brand
+      href="#homepage-top-row"
+      @click="scrollIntoView"><logo/></b-navbar-brand>
+
+    <b-navbar-toggle target="nav_collapse"/>
+
+    <b-collapse
+      id="nav_collapse"
+      is-nav>
+
+      <b-navbar-nav>
+        <b-nav-item to="/" >Home</b-nav-item>
+        <b-nav-item to="/about" >About</b-nav-item>
+        <b-nav-item href="/#services" >Services</b-nav-item>
+        <b-nav-item href="/#technology">Technology</b-nav-item>
+        <b-nav-item
+          href="#"
+          disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-form>
+          <b-form-input
+            size="sm"
+            class="mr-sm-2"
+            type="text"
+            placeholder="Search"/>
+          <b-button
+            size="sm"
+            class="my-2 my-sm-0"
+            type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown
+          text="Lang"
+          right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: rgba(0, 0, 0, 0.5);
+}
+</style>
